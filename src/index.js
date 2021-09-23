388126b2f43fdd899d0b50ad0289dcab42c8c364
Willy Ardiano <willyjessiel99@gmail.com>
2021-09-23 12:38:31 -0600

    Agregando respuesta api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require( "express" );
 const app = express();
-const port = 8080; // default port to listen
+const port = 9091; // default port to listen
+
+app.use((request,response,next)=>{
+    console.log("Hello from middleware");
+    next();
+});
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+app.get( "/", ( request, response ) => {
+    console.log("Serving the response");
+    response.send( "Hello world from 9090!" );
 } );
+
+app.post("/api/calc",(request,response)=>{
+    response.send("Api works");
+});
 
 // start the Express server
 app.listen( port, () => {
